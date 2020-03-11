0d6c82a577df7f77c96b6c4250f284fd48299445
Joe Appleton <joe.appleton@solent.ac.uk>
2020-03-11 09:59:24 +0000

    twitter update

--- week_8/main.js
+++ week_8/main.js
@@ -4,16 +4,26 @@
  * I should be able to send a tweet
  */
 var tweetInput;
+var tweetLimit = 10;
 
 function main() {
     tweetInput =  document.getElementById('tweetInput');
+    
     tweetInput.addEventListener('input', handleChange);
 }
 
 function handleChange(e) {
-    debugger;
-    document.getElementById('letter-count')
-        .innerHTML =   tweetInput.value.length;
+    // githubcom/joeappleton18/web-dev 
+    var tweetLength = tweetInput.value.length;
+    var letterCount =   document.getElementById('letter-count');
+   
+    if(tweetLength >= tweetLimit) {
+        letterCount.style.color = "red";
+    } else {
+        letterCount.style.color = "";
+    }
+    
+    letterCount.innerHTML =  tweetLength;
                    
 }
 
