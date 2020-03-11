01877eafe606e6360f00890f9e31804569b0e8ed
Joe Appleton <joe.appleton@solent.ac.uk>
2020-03-11 10:34:34 +0000

    twitter update

--- week_8/main.js
+++ week_8/main.js
@@ -5,12 +5,38 @@
  */
 var tweetInput;
 var tweetLimit = 10;
+var tweets = [];
+
+
 
 function main() {
     tweetInput =  document.getElementById('tweetInput');
-    
     tweetInput.addEventListener('input', handleChange);
+    document.addEventListener('keydown', handleKeyDown);
+
 }
+
+function handleKeyDown(e) {
+    // "Enter"
+    var tweetOutput = "";
+    if (e.code === "Enter") {
+        // only tweet if text is within length
+        if (tweetInput.value.length <= tweetLimit) {
+            tweets.push(tweetInput.value);
+            tweetInput.value = "";
+            for (var i = 0; i < tweets.length; i++) {
+                tweetOutput += tweets[i] + " ";
+            }
+            
+            document.getElementById('tweetOutput').innerHTML =  tweetOutput;
+
+        }
+        
+       
+    }
+    
+}
+  
 
 function handleChange(e) {
     // githubcom/joeappleton18/web-dev 
