143d8be85bc69a612a6575ce1427742ebc949296
Joe Appleton <joe.appleton@solent.ac.uk>
2020-03-11 11:27:17 +0000

    created main function

--- week_8/main.js
+++ week_8/main.js
@@ -1,7 +1,10 @@
-var tweetCount;
-
+var tweetCount, tweetInput;
+// set up a function called handleInput console.log('changed')
 function main() {
     tweetCount = document.getElementById('tweet-count');
-    debugger;
+    tweetInput = document.getElementById('tweet-input');
+ 
+    tweetInput.addEventListener('input', handleInput)
+    
     console.info('page has loaded');
 }
